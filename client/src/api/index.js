5dac295538f7ed8c0730f21cfbf43599beeffd4b
pragti-chauhan <pragtichauhan1541@gmail.com>
2023-07-06 19:37:10 +0530

    Modify index file in api/client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://mellow-clafoutis-e7ef76.netlify.app/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
